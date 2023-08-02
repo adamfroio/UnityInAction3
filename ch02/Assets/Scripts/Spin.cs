1861ce1e403c2e52e0b7e6ba9fc80db244568970
Adam Froio <afroio@gmail.com>
2023-08-01 17:23:17 -0700

    added enemy and fireball prefabs

---
+++ ch02/Assets/Scripts/Spin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public float speed = 0.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 1, 0);    
+    }
+}
